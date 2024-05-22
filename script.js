4690787685e49f5bb18277f30b5043ad471bdf03
Thiago00111000 <127748415+Thiago00111000@users.noreply.github.com>
2024-05-22 09:35:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
 //import './style.css';
-//import {resources} from "resource.js";
-
-class resources {
-    constructor(){
-        //para baixar
-        this.toLoad = {
-            background: "./Images/background.jpg",
-        };
-        //lista de assets
-        this.images = {};
-        Object.keys(this.images).forEach(key => {
-            const img = new Image();
-            img.src = this.toLoad[key];
-            this.images[key] = {
-                image: img,
-                isLoaded: false
-            }
-            img.onLoad = () => {
-                this.images[key].isLoaded = true
-            }
-        });
-    }
-}
 
 //------//
 
-canvas = document.getElementById('grid');
+const canvas = document.getElementById('grid');
 const ctx = canvas.getContext("2d");
 
-const draw = () => {
-    const background = resources.images.background;
-        if (background.isLoaded){
-        ctx.drawImage(background.image, 0, 0)
+const size = 30;
+const snake = [
+    {x: 300, y: 300},
+    {x: 330, y: 300},
+    {x: 360, y: 300},
+    {x: 390, y: 300},
+    {x: 420, y: 300}
+]
+
+let direction,loopId
+
+const drawSnake = () => {
+    ctx.fillStyle = "lightgrey"
+    snake.forEach((position, index)=>{
+        if(index == snake.length - 1 ){
+            ctx.fillStyle = "white"
         }
+        ctx.fillRect(position.x,position.y, size, size)
+    })
 }
 
-setInterval(()  => {
-    draw();
-}, 300);
+const moveSnake = () => {
+    if(!direction) return
+    const head = snake[snake.length - 1]
+
+    snake.shift()
+    
+    if (direction == "right"){
+        snake.push({x: head.x + size , y: head.y})
+    }
+    if (direction == "left"){
+        snake.push({x: head.x - size , y: head.y})
+    }if (direction == "up"){
+        snake.push({x: head.x , y: head.y - size})
+    }if (direction == "down"){
+        snake.push({x: head.x , y: head.y + size})
+    }
+}
+
+const loop = () => {
+    clearInterval(loopId)
+    ctx.clearRect(0,0,600,600)
+    moveSnake()
+    drawSnake()
+    
+    loopId = setTimeout(() => {
+        loop()         
+    }, 300);
+}
+
+loop()
+
+document.addEventListener("keydown", ({key}) => {
+    if(key == "w" && direction  !=  "down"){
+        direction = "up"
+    }
+    if(key == "s" && direction  !=  "up"){
+        direction = "down"
+    }
+    if(key == "a" && direction  !=  "right"){
+        direction = "left"
+    }
+    if(key == "d" && direction  !=  "left"){
+        direction = "right"
+    }
+})
+//cronometro
