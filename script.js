17b7509ed8b5da477f0b3ed63e9b91fe214cc3a6
Thiago00111000 <127748415+Thiago00111000@users.noreply.github.com>
2024-05-27 09:40:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,9 +44,28 @@ const moveSnake = () => {
     }
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30){
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+        
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }
+
+}
+
 const loop = () => {
     clearInterval(loopId)
     ctx.clearRect(0,0,600,600)
+    drawGrid()
     moveSnake()
     drawSnake()
     
